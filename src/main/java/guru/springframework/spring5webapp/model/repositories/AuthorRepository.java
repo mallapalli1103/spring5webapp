1e6b52c4486c01f92e00c3739152efbad6342363
Madhuram <mallapalli.313@gmail.com>
2019-05-30 19:30:05 +0530

    Added Publisher Entity

---
+++ src/main/java/guru/springframework/spring5webapp/model/repositories/AuthorRepository.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapp.model.repositories;
+
+import guru.springframework.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+
+
+
+
+
+}
